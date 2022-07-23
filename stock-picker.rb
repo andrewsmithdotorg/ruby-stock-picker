99bdbe710485fc8b2ad6435424f486bf20b65c25
Andrew <andrew@andrewsmith.org>
2022-07-23 02:51:32 -0400

    Add working solution

--- stock-picker.rb
+++ stock-picker.rb
@@ -3,12 +3,15 @@ prices = [17, 3, 6, 9, 15, 8, 6, 1, 10]
 def stock_picker (prices)
     buy_options = prices[0..-2]
     sell_options = prices[1..-1]
-    arr = []
-    buy_options.each_with_index do |buy_price, index|
-        sell_options[index..-1].each do |sell_price|
-            puts sell_price - buy_price
+    best = [0, 0, 0] # [profit, buy day, sell day]
+    buy_options.each_with_index do |buy_price, buy_index|
+        sell_options[buy_index..-1].each_with_index do |sell_price, sell_index|
+            if (sell_price - buy_price) > best[0]
+                best = [(sell_price - buy_price), (buy_index), (sell_index + buy_index + 1)]
+            end
         end
     end
+    return best[1..-1]
 end
 
-stock_picker(prices)
+puts stock_picker(prices)
